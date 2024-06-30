779ea5907bdaecc8f5f50371822293b6c0f7e04e
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-07-01 00:54:07 +0300

    chore: Update Braintree configuration in CoursesApplication

--- src/main/java/com/web/CoursesQuiz/CoursesApplication.java
+++ src/main/java/com/web/CoursesQuiz/CoursesApplication.java
@@ -15,33 +15,23 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Courses REST API Documentation",
-				description = "Math Courses REST API Documentation",
-				version = "v1",
-				contact = @Contact(
-						name = "Mostafa Mohamed",
-						email = "mostafa19500mahmoud@gmail.com"
-				)
-		)
-)
+@OpenAPIDefinition(info = @Info(title = "Courses REST API Documentation", description = "Math Courses REST API Documentation", version = "v1", contact = @Contact(name = "Mostafa Mohamed", email = "mostafa19500mahmoud@gmail.com")))
 public class CoursesApplication {
 	public static String DEFAULT_CONFIG_FILENAME = "config.properties";
-    public static BraintreeGateway gateway;
+	public static BraintreeGateway gateway;
 
 	public static void main(String[] args) {
-		// File configFile = new File(DEFAULT_CONFIG_FILENAME);
-        // try {
-        //     if(configFile.exists() && !configFile.isDirectory()) {
-        //         gateway = BraintreeGatewayFactory.fromConfigFile(configFile);
-        //     } else {
-        //         gateway = BraintreeGatewayFactory.fromConfigMapping(System.getenv());
-        //     }
-        // } catch (NullPointerException e) {
-        //     System.err.println("Could not load Braintree configuration from config file or system environment.");
-        //     System.exit(1);
-        // }
+		File configFile = new File(DEFAULT_CONFIG_FILENAME);
+		try {
+			if (configFile.exists() && !configFile.isDirectory()) {
+				gateway = BraintreeGatewayFactory.fromConfigFile(configFile);
+			} else {
+				gateway = BraintreeGatewayFactory.fromConfigMapping(System.getenv());
+			}
+		} catch (NullPointerException e) {
+			System.err.println("Could not load Braintree configuration from config file or system environment.");
+			System.exit(1);
+		}
 		SpringApplication.run(CoursesApplication.class, args);
 	}
 	// sudo systemctl start mongod
